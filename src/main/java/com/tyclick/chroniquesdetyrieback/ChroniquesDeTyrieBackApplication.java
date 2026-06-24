0ab66a87569a15de2cce80c8d48de32da8a046b9
Thomas MARTIN <thomas-martin35@hotmail.fr>
2026-06-24 10:40:15 +0200

    chore: initialize Spring Boot project

---
+++ src/main/java/com/tyclick/chroniquesdetyrieback/ChroniquesDeTyrieBackApplication.java
@@ -0,0 +1,13 @@
+package com.tyclick.chroniquesdetyrieback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChroniquesDeTyrieBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChroniquesDeTyrieBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tyclick/chroniquesdetyrieback/ChroniquesDeTyrieBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyclick.chroniquesdetyrieback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChroniquesDeTyrieBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
